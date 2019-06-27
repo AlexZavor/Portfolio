990db167f534c1fb6c363355679dfa754cdf33ae
ALEX CARTER <carteral2@wsd.ad>
2019-06-27 10:56:23 -0600

    Navigation container

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from './navigation/Navigation-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>This is the beggining!</h1>
         <h2>prepare the portfolio</h2>
         <div>
