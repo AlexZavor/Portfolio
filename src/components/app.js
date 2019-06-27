91e0be785ea712123610c4e7e964bf07bd1cb9d2
ALEX CARTER <carteral2@wsd.ad>
2019-06-27 11:31:54 -0600

    routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from './navigation/Navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <NavigationContainer />
+
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path = "/" Component = {Home} />
+              <Route path = "/about-me" Component = {About} />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>This is the beggining!</h1>
         <h2>prepare the portfolio</h2>
         <div>
