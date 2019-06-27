ea17f6e8419059bf378fb933554540b138aefd97
ALEX CARTER <carteral2@wsd.ad>
2019-06-27 08:17:03 -0600

    go!

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from './portfolio-container'
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>This is the beggining!</h1>
         <h2>prepare the portfolio</h2>
+        <PortfolioContainer />
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import react, {component} from "react";
+
+export default class PortfolioContainer extends Component{
+    Render() {
+        return(
+            <div>
+                <h2>Portfolio items go here...</h2>
+            </div>
+        )
+    }
+}
