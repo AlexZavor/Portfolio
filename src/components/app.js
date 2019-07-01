412bd9097578199b93b9399be26545f995b4fa0c
ALEX CARTER <carteral2@wsd.ad>
2019-07-01 11:38:18 -0600

    container styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from 'moment';
 import {
   BrowserRouter as Router,
   Switch,
@@ -17,13 +16,7 @@ import NoMatch from './pages/no-match';
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-
-        <h1>Alex Carter's Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
-
+      <div className='container'>
         <Router>
           <div>
             <NavigationContainer />
