74c6474f47a1143e32ae54333521617353eacee0
ALEX CARTER <carteral2@wsd.ad>
2019-07-01 11:21:31 -0600

    style nav bar

--- src/components/navigation/Navigation-container.js
+++ src/components/navigation/Navigation-container.js
@@ -8,14 +8,27 @@ export default class NavigationContainer extends Component {
 
     render(){
         return(
-            <div>
+            <div className="nav-wrapper">
+                <div className="left-side">
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to="/">Home</NavLink>
+                    </div>
 
-                <NavLink exact to="/">Home</NavLink>
-                <NavLink to="/about-me">About</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-                <NavLink to="/Blog">Blog</NavLink>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/about-me">About</NavLink>
+                    </div>
 
-                {false ? <button>Add Blog</button> : null}
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/contact">Contact</NavLink>
+                    </div>
+
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/Blog">Blog</NavLink>
+                    </div>
+                </div>
+                <div className="right-side">
+                    ALEXANDER CARTER
+                </div>
             </div>
         );
     }
