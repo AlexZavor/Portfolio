42b06a011d44beacc15d59bb4c33bf07b59e1bb3
ALEX CARTER <carteral2@wsd.ad>
2019-07-01 09:58:06 -0600

    axios api

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import PortfolioItem from './portfolio-item';
 
 export default class PortfolioContainer extends Component {
+  
     constructor() {
         super();
 
         this.state = {
             pageTitle: "Welcome to the portfolio",
             isLoading: false,
-            data: [
-                {title: 'Alex', catagory: "eCommerce", slug: 'Alex'}, 
-                {title: 'Nate', catagory: "Scheduling", slug: 'Nate'}, 
-                {title: 'no u', catagory: "enterprize", slug: 'No-u'}, 
-                {title: 'ext.', catagory: "eCommerce", slug: 'ext'} 
-            ]
+            data: []
         }
 
         this.handleFilter = this.handleFilter.bind(this);
     }
+  
+    getPortfolioItems(){
+        axios
+          .get('https://acarter.devcamp.space/portfolio/portfolio_items')
+          .then(responce => {
+            this.setState({
+                data: responce.data.portfolio_items
+            })
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
 
     portfolioItems(){
         return this.state.data.map(
             item => {
-                return <PortfolioItem title = {item.title} slug={item.slug} />;
+                console.log("Portfolio item", item);
+                return <PortfolioItem 
+                    key = {item.id} 
+                    title = {item.name} 
+                    url={item.url} 
+                    slug={item.id} 
+                />;
             }
         );
+    }
+
+    componentDidMount(){
+        this.getPortfolioItems();
     }
 
     handleFilter(filter){
@@ -40,7 +60,6 @@ export default class PortfolioContainer extends Component {
         if (this.state.isLoading) {
             return <div>Loading...</div>
         }
-
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -5,6 +5,7 @@ export default function(props){
     return(
         <div>
             <h3>{props.title}</h3>
+            <h4>{props.url}</h4>
             <Link to={`/Portfolio/${props.slug}`}>Link</Link>
         </div>
     );
