5934a1324b3c644640491f95f4cadb9ee90c1899
ALEX CARTER <carteral2@wsd.ad>
2019-07-02 09:00:18 -0600

    home page!

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function(props){
+debugger;
 
-    const{ id, description, thumb_image_url, logo} = props.item
-
-    return(
+export default function(props) {
+    const { id, description, thumb_image_url, logo_url } = props.item;
+    return (
         <div className="portfolio-item-wrapper">
             <div 
                 className="portfolio-img-background"
                 style={{
-                    backgorundImage: "url(" + thumb_image_url + ")"
+                    backgroundImage: "url(" + thumb_image_url + ")"
                 }}
             />
-            <img src={logo}/>
-            <div>{description}</div>
-            <Link to={`/Portfolio/${id}`}>Link</Link>
+            <div className="img-text-wrapper">
+                <div className="logo-wrapper">
+                    <img src={logo_url} />
+                </div>
+                <div className="subtitle">{description}</div>
+            </div>
         </div>
     );
 }
