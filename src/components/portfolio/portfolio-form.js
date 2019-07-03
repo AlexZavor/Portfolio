832e74b14ce40bc508d386e5b7c88eccf5eac6ec
ALEX CARTER <carteral2@wsd.ad>
2019-07-03 11:40:10 -0600

    form build

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,16 +1,84 @@
 import React, {Component} from 'react';
 
 export default class PortfolioForm extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state ={
+            name: "",
+            description: "",
+            catagory: "",
+            position: "",
+            url: "",
+            thumb_image: "",
+            banner_image: "",
+            logo: "",
+        };
 
-        this.state
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event){
+        console.log("event", event)
+        event.preventDefault();
     }
 
     render(){
         return(
             <div>
-                <h1>portfolio form</h1>
+                <h1>Portfolio Form</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <input
+                        type='text'
+                        name='name'
+                        placeholder='Portfolio Item Name'
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        />
+                        <input
+                        type='text'
+                        name='url'
+                        placeholder='URL'
+                        value={this.state.url}
+                        onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <input
+                        type='text'
+                        name='position'
+                        placeholder='Postion'
+                        value={this.state.position}
+                        onChange={this.handleChange}
+                        />
+                        <input
+                        type='text'
+                        name='catagory'
+                        placeholder='Catagory'
+                        value={this.state.catagory}
+                        onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <input
+                        type='text'
+                        name='description'
+                        placeholder='Description'
+                        value={this.state.description}
+                        onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">Save</button>
+                    </div>
+                </form>
             </div>
         );
     }
