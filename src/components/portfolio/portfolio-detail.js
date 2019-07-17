57f98387938dc1edcd9c3360aa50fba24450f4ee
ALEX CARTER <carteral2@wsd.ad>
2019-07-17 09:55:47 -0600

    pull data into portfolio detail page

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-export default function (props) {
-    return (
-        <div>
-            <h2>Portfolio detail for {props.match.params.slug}</h2>
-        </div>
-    );
+export default class PortfolioDetail extends Component {
+    constructor(props){
+        super(props);
+
+        this.state ={
+            data: []
+        }
+
+        this.getPortfolioItem = this.getPortfolioItem.bind(this);
+    }
+
+    getPortfolioItem(){
+        axios.get(`https://acarter.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+        {withCredentials: true})
+        .then(response => {
+            console.log("res", response);
+        }).catch(error => {
+            console.log('get portfolio item error', error);
+        });
+
+    }
+
+    componentWillMount(){
+        this.getPortfolioItem()
+    }
+
+    render(){
+        return (
+            <div>
+                <h2>Portfolio detail for {this.props.match.params.slug}</h2>
+    
+            </div>
+        );
+    }
 }
